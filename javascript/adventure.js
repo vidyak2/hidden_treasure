e4e334b6011ee999336a2efbbc3296fa83c31afd
vidyak2 <vidyakrishnan.vk@gmail.com>
2018-02-02 14:09:05 -0800

    Add start game button, random location choice, and bigger board

--- javascript/adventure.js
+++ javascript/adventure.js
@@ -1,20 +1,32 @@
 //Define the size of the game play area (the board)
-var maxX = 2;
-var maxY = 2;
+//var maxX = 2;
+//var maxY = 2;
 
-//Make the game area larger: they now have 100 spaces to play in
+//Extra challenge: Make the game area larger: they now have 100 spaces to play in
+var maxX = 6;
+var maxY = 6;
 
 //(x,y) grid, with first square = (0,0)
 var userX = 0;
 var userY = 0;
 
 //Hide the treasure, also with (x,y) values, not randomized yet
-//var treasureX = 2;
-//var treasureY = 1;
+var treasureX = 2;
+var treasureY = 1;
 
-//Hide treasure in random (x,y) values between 0 and 99
-//var treasureX = Math.floor(Math.random() * 10);
-//var treasureY = Math.floor(Math.random() * 10);
+//Extra challenge: Define variables to start game, and create a starting button attached to the html function "start"
+var currentlyplaying = false;
+var startingbutton;
+
+startingbutton = start.add.button()
+
+//Extra challenge: Hide treasure in random (x,y) values between 0 and 5
+var treasureX = Math.floor(Math.random() * 6);
+var treasureY = Math.floor(Math.random() * 6);
+
+//So we can keep track of the treasure in console.log
+console.log(treasureX)
+console.log(treasureY)
 
 //Main loop control
 var treasureFound = false;
@@ -22,69 +34,73 @@ var treasureFound = false;
 //Get user's name
 var name = prompt("Welcome brave adventurer! What is your name?");
 
-//You can add "Hello ____, where would you want to move?"
-while (!treasureFound) {
-  var direction = prompt("Which direction would you like to go in? (North, South, East, West)")
-  console.log(direction);
+function start() {
 
-  //Temporary vars only used for checking the validity of the new user location after move
-  var newX
-  var newY
+  //You can add "Hello ____, where would you want to move?"
+  while (!treasureFound) {
+    var direction = prompt("Which direction would you like to go in? (North, South, East, West)")
+    console.log(direction);
 
-  //See what new user location should be
+    //Extra challenge: Input is now case insensitive, so user can input direction name with any case
+    var directioncaseinsensitive = direction.toLowerCase();
+    console.log(directioncaseinsensitive);
 
-  //Check if new user location is valid
+    //Temporary vars only used for checking the validity of the new user location after move
+    var newX
+    var newY
 
-  //Check if new user location is treasure
-
-  if (direction == "north") {
-    newX = userX;
-    newY = userY + 1;
-    //neither value <0, neither is > max
-    if (newX >= 0 && newY >= 0 && newX <= maxX && newY <= maxY) {
-      userX = newX;
-      userY = newY;
+    //See what new user location should be
+    if (directioncaseinsensitive == "north") {
+      newX = userX;
+      newY = userY + 1;
+      //Check if new user location is valid
+      //neither value <0, neither is > max
+      if (newX >= 0 && newY >= 0 && newX <= maxX && newY <= maxY) {
+        userX = newX;
+        userY = newY;
+      } else {
+        console.log("There is a forbidden mountain range in that direction, you cannot go!!")
+      }
+    } else if (directioncaseinsensitive == "east") {
+      newX = userX + 1;
+      newY = userY;
+      //neither value <0, neither is > max
+      if (newX >= 0 && newY >= 0 && newX <= maxX && newY <= maxY) {
+        userX = newX;
+        userY = newY;
+      } else {
+        console.log("There is a forbidden mountain range in that direction, you cannot go!!")
+      }
+    } else if (directioncaseinsensitive == "south") {
+      newX = userX;
+      newY = userY - 1;
+      //neither value <0, neither is > max
+      if (newX >= 0 && newY >= 0 && newX <= maxX && newY <= maxY) {
+        userX = newX;
+        userY = newY;
+      } else {
+        console.log("There is a forbidden mountain range in that direction, you cannot go!!")
+      }
+    } else if (directioncaseinsensitive == "west") {
+      newX = userX - 1;
+      newY = userY;
+      //neither value <0, neither is > max
+      if (newX >= 0 && newY >= 0 && newX <= maxX && newY <= maxY) {
+        userX = newX;
+        userY = newY;
+      } else {
+        console.log("There is a forbidden mountain range in that direction, you cannot go!!")
+      }
     } else {
-      console.log("There is a forbidden mountain range in that direction, you cannot go!!")
+      console.log("Please enter a valid direction")
     }
-  } else if (direction == "east") {
-    newX = userX + 1;
-    newY = userY;
-    //neither value <0, neither is > max
-    if (newX >= 0 && newY >= 0 && newX <= maxX && newY <= maxY) {
-      userX = newX;
-      userY = newY;
-    } else {
-      console.log("There is a forbidden mountain range in that direction, you cannot go!!")
+    //Check if new user location is treasure
+    //See if user location matches treasure
+    if (userX == treasureX && userY == treasureY) {
+      treasureFound = true;
+
+      console.log("You won!")
     }
-  } else if (direction == "south") {
-    newX = userX;
-    newY = userY - 1;
-    //neither value <0, neither is > max
-    if (newX >= 0 && newY >= 0 && newX <= maxX && newY <= maxY) {
-      userX = newX;
-      userY = newY;
-    } else {
-      console.log("There is a forbidden mountain range in that direction, you cannot go!!")
-    }
-  } else if (direction == "west") {
-    newX = userX - 1;
-    newY = userY;
-    //neither value <0, neither is > max
-    if (newX >= 0 && newY >= 0 && newX <= maxX && newY <= maxY) {
-      userX = newX;
-      userY = newY;
-    } else {
-      console.log("There is a forbidden mountain range in that direction, you cannot go!!")
-    }
-  } else {
-    console.log("Please enter a valid direction")
   }
 
-  //See if user location matches treasure
-  if (userX == treasureX && userY == treasureY) {
-    treasureFound = true;
-
-    console.log("You won!")
-  }
 }
---
+++ javascript/trialgame.js
@@ -0,0 +1,20 @@
+////Creating a game canvas
+//function game() {
+//  //  gamearea.start();
+//}
+
+//var gamearea = {
+//  canvas: document.createElement("canvas"),
+//  start: function () {
+//    this.canvas.width = 500;
+//    this.canvas.height = 500;
+//    this.context = this.canvas.getContext("2d");
+//
+//    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+//  }
+//}
+
+//var canvas = document.getElementById("mycanvas");
+//var ctx = canvas.getContext("2d");
+//ctx.fillStyle = "#FF0000";
+//ctx.fillRect(399, 399, 1, 1);
